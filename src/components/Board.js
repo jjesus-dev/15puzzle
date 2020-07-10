1e1e57505d80baaa403165b69423f1f08c4cdd73
Javier Jesús Martínez Moreno <jj_dev@outlook.com>
2020-07-09 20:56:27 -0500

    Change function to move numbers and save their state

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,34 +5,39 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      numbers: Array(3).fill(null),
+      //numbers: Array(4).fill(null),
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
     };
   }
 
-  renderNumber(i) {
-    return (
-      <Square
-        value={this.state.numbers[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
-  }
+  changeNumber = (index) => {
+    const arrangedNumbers = this.state.numbers;
+    let movedNumber;
+    // Find the blank square (16)
+    const blankIndex = arrangedNumbers.indexOf(16);
 
-  handleClick(i) {
-    const numbers = this.state.numbers.slice();
-    numbers[i] = "X";
-    this.setState({ numbers, numbers });
-  }
+    if (arrangedNumbers[index] !== 16) {
+      console.log(arrangedNumbers[index] + ": Can move.");
+      movedNumber = arrangedNumbers[index];
+      arrangedNumbers.splice(blankIndex, 1, movedNumber);
+      arrangedNumbers.splice(index, 1, 16);
+
+      this.setState({
+        numbers: arrangedNumbers,
+      });
+    } else {
+      console.log("Can't move the same number.");
+    }
+  };
 
   render() {
-    const title = "Move the numbers";
+    const title = "Arrange the numbers";
+    const newNumbers = this.state.numbers;
 
     return (
       <div>
         <div>{title}</div>
-        <div>{this.renderNumber(0)}</div>
-        <div>{this.renderNumber(1)}</div>
-        <div>{this.renderNumber(2)}</div>
+        <Square numbers={newNumbers} changeNumber={this.changeNumber} />
       </div>
     );
   }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,13 +1,25 @@
 import React from "react";
 
 class Square extends React.Component {
+  renderButtons(props) {
+    const cells = props.numbers.map((cell, index) => {
+      return (
+        <li key={index}>
+          {/*[{index}]&gt;*/}
+          <button className="number" onClick={() => props.changeNumber(index)}>
+            {cell}
+          </button>
+        </li>
+      );
+    });
+
+    return <ul className="list-row">{cells}</ul>;
+  }
+
   render() {
-    //console.log(this.props.value);
-    return (
-      <div className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </div>
-    );
+    const numbers = this.props;
+
+    return <div>{this.renderButtons(numbers)}</div>;
   }
 }
 
